2aee6d0b91a8c6039d7ae5ee814a55867dc5beb1
gfluxai <gflux.ai@gmail.com>
2026-02-27 13:55:05 -0300

    Update 20231211010807_add_cached_labels_list.rb

--- db/migrate/20231211010807_add_cached_labels_list.rb
+++ db/migrate/20231211010807_add_cached_labels_list.rb
@@ -1,7 +1,12 @@
 class AddCachedLabelsList < ActiveRecord::Migration[7.0]
   def change
     add_column :conversations, :cached_label_list, :string
-    Conversation.reset_column_information
-    ActsAsTaggableOn::Taggable::Cache.included(Conversation)
+
+    # compat: acts-as-taggable-on mudou o módulo
+    if defined?(ActsAsTaggableOn::Taggable::Cache)
+      ActsAsTaggableOn::Taggable::Cache.included(Conversation)
+    elsif defined?(ActsAsTaggableOn::Taggable::CacheKeys)
+      ActsAsTaggableOn::Taggable::CacheKeys.included(Conversation)
+    end
   end
 end
