3ac0b92679c67403ae2f5371e235f76f2ee4ab3f
gfluxai <gflux.ai@gmail.com>
2026-02-27 13:26:50 -0300

    Update 20231211010807_add_cached_labels_list.rb

--- db/migrate/20231211010807_add_cached_labels_list.rb
+++ db/migrate/20231211010807_add_cached_labels_list.rb
@@ -1,7 +1,7 @@
 class AddCachedLabelsList < ActiveRecord::Migration[7.0]
   def change
-    unless column_exists?(:conversations, :cached_label_list)
-      add_column :conversations, :cached_label_list, :string
-    end
+    add_column :conversations, :cached_label_list, :string
+    Conversation.reset_column_information
+    ActsAsTaggableOn::Taggable::Cache.included(Conversation)
   end
 end
