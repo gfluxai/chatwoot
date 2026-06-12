fceec7db95e674c308168ad08987bcf125fd3a83
gmonteirodossa <gabrielmonteiro.g10@gmail.com>
2026-06-12 06:16:18 -0300

    Rename @token ivar in TabloTokensController to avoid DeviseTokenAuth collision

--- app/controllers/api/v1/accounts/tablo_tokens_controller.rb
+++ app/controllers/api/v1/accounts/tablo_tokens_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Accounts::TabloTokensController < Api::V1::Accounts::BaseController
   def show
-    @token = Tablo::SupabaseTokenService.new(user: current_user).generate
+    @tablo_token = Tablo::SupabaseTokenService.new(user: current_user).generate
 
-    if @token.blank?
+    if @tablo_token.blank?
       head :not_found
       return
     end
 
-    render json: @token
+    render json: @tablo_token
   end
 end
